81b2cc2d9e0957102ce7b3ccc380a47da608b474
Kushagra Mishra <kmishra@ncsu.edu>
2017-04-15 11:45:25 -0400

    private network commands added

--- scripts/ovs_setup.py
+++ scripts/ovs_setup.py
@@ -19,6 +19,39 @@ def add_new_user(user_name):
 	# Start a ovs shell
 	subprocess.call(['su', '-', user_name])
 
+def ovs_xml_config(file, network, bridge):
+	file = open(file, 'w+')
+	file.write("<network>\n")
+	file.write("<name>%s</name>\n" % network)
+	file.write("<forward mode='bridge'/>\n")
+	file.write("<bridge name='%s'/>\n" % bridge)
+	file.write("<virtualport type='openvswitch'/>")
+	file.write("</network>")
+	file.close()
+
+# def create_backup(network):
+
+def destroy_old_network(network):
+	cmd_destroy = ['virsh', 'net-destroy', network]
+	cmd_undefine = ['virsh', 'net-undefine', network]
+	subprocess.call(cmd_destroy)
+	subprocess.call(cmd_undefine)
+
+def start_new_network(network, network_config_path):
+	cmd_define = ['virsh', 'net-define', network_config_path]
+	cmd_autostart = ['virsh', 'net-autostart', network]
+	cmd_start = ['virsh', 'net-define', network]
+	subprocess.call(cmd_destroy)
+
+def add_new_bridge(bridge):
+	cmd_new_bridge = ['ovs-vsctl', 'add-br', bridge]
+	subprocess.call(cmd_new_bridge)
+
+def create_ovs_network(network):
+	destroy_old_network("private")
+	ovs_xml_config("/tmp/new_config.xml", network, "ovsbr0")
+	add_new_bridge("ovsbr0")
+	start_new_network(network, "/tmp/new_config.xml")
 
 if __name__ == "__main__":
 
@@ -27,8 +60,9 @@ if __name__ == "__main__":
 
 	add_new_user("ovs")
 
-
 	# Add two bridges 
+	create_ovs_network("private")
 
+	
 
 
